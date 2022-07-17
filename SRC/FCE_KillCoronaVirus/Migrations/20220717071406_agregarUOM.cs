362227e71336d36e3974f8ef47d451fcf4efd6e7
Natalia <nat.otarola@gmail.com>
2022-07-17 03:16:30 -0400

    agregar UOM

---
+++ SRC/FCE_KillCoronaVirus/Migrations/20220717071406_agregarUOM.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCE_KillCoronaVirus.Migrations
+{
+    public partial class agregarUOM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Uom",
+                table: "UnidadDeMedida",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nomFar",
+                table: "Farmaco",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uom",
+                table: "UnidadDeMedida");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nomFar",
+                table: "Farmaco",
+                type: "varchar(50)",
+                unicode: false,
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldUnicode: false,
+                oldMaxLength: 50);
+        }
+    }
+}
